03e1d1b8108b6a831b2a048451f9d0128251d2fd
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-17 15:22:35 +0200

    initial commit

---
+++ backend/src/test/java/net/schwarzbaer/spring/prompttester/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package net.schwarzbaer.spring.prompttester.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
