8f0f3453a8c73172305099889ebd65bc2bb3f1dc
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-17 23:15:52 +0200

    setup: test deployment tool chain

--- backend/src/main/java/net/schwarzbaer/spring/prompttester/backend/HelloController.java
+++ backend/src/main/java/net/schwarzbaer/spring/prompttester/backend/HelloController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
 	@GetMapping("/hello")
 	public String getHelloWorld(){
-		return "Hello World :)";
+		return "Hello World :)  [ %s ]".formatted(ZonedDateTime.now().toString());
 	}
 
 }
