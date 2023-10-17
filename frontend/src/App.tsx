5f563b6658ffe412461d9e3a0f4cc791aa89feba
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-17 15:48:46 +0200

    setup: configured MongoDB, added small connection check frontend to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import HelloWorld from "./HelloWorld.tsx";
 
 export default function App() {
 
-  return (
-    <>
-      <h1>HeadLine</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>HeadLine</h1>
+            <HelloWorld/>
+        </>
+    )
 }
 
---
+++ frontend/src/HelloWorld.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+export default function HelloWorld() {
+    const [output, setOutput] = useState<string>("????")
+
+    useEffect( getAll, [] )
+
+    function getAll() {
+        axios
+            .get('/api/hello')
+            .then(response => {
+                if (response.status != 200)
+                    throw new Error(`Got wrong status on load data: ${response.status}`);
+                return response.data;
+            })
+            .then(data => {
+                console.debug("HelloWorld -> data loaded", data);
+                setOutput(data)
+            })
+            .catch(reason => {
+                console.error(reason);
+            })
+    }
+
+    return (
+        <>
+            <h4>Output: {output}</h4>
+        </>
+    )
+}
