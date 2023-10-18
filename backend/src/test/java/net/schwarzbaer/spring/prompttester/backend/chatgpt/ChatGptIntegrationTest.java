2da38413680f7c3d01ba8097e3eb7d3044866f0f
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-18 15:59:16 +0200

    feat: added integration test

---
+++ backend/src/test/java/net/schwarzbaer/spring/prompttester/backend/chatgpt/ChatGptIntegrationTest.java
@@ -0,0 +1,91 @@
+package net.schwarzbaer.spring.prompttester.backend.chatgpt;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ChatGptIntegrationTest {
+
+	private static MockWebServer mockWebServer;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeAll
+	static void setup() throws IOException {
+		mockWebServer = new MockWebServer();
+		mockWebServer.start();
+	}
+
+	@AfterAll
+	static void teardown() throws IOException {
+		mockWebServer.shutdown();
+	}
+
+	@DynamicPropertySource
+	static void setUrlDynamically(DynamicPropertyRegistry reg) {
+		reg.add("app.openai-api-key", () -> "dummy_api_key");
+		reg.add("app.openai-api-org", () -> "dummy_api_org");
+		reg.add("app.openai-api-url", () -> mockWebServer.url("/").toString());
+	}
+
+	@Test
+	void whenAskChatGPT_getsAPrompt_returnsAnAnswer() throws Exception {
+		// Given
+		mockWebServer.enqueue(
+				new MockResponse()
+						.setHeader("Content-Type", "application/json")
+						.setBody("""
+								{
+									"choices": [
+										{
+											"message": {
+												"content": "TestAnswer"
+											}
+										}
+									],
+									"usage": {
+										"total_tokens": 35
+									}
+								}
+								""")
+		);
+
+		// When
+		mockMvc
+				.perform(MockMvcRequestBuilders
+						.post("/api/ask")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("""
+								{
+									"prompt": "TestPrompt"
+								}
+								""")
+				)
+
+				// Then
+				.andExpect(status().isOk())
+				.andExpect(content().json("""
+								{
+									"answer": "TestAnswer"
+								}
+								"""));
+	}
+}
