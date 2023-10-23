e157f5f44844286255229ff49202f16b504ee5d6
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-23 13:04:42 +0200

    feat: [UserManagement] added some user details

--- backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/security/UserController.java
+++ backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/security/UserController.java
@@ -1,16 +1,43 @@
 package net.schwarzbaer.spring.promptoptimizer.backend.security;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.PrintStream;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+	private static final PrintStream DEBUG_OUT = System.out;
 
 	@GetMapping("me")
-	public String getMe() {
-		return SecurityContextHolder.getContext().getAuthentication().getName();
+	public UserInfos getMe() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		DEBUG_OUT.println("Authentication: "+authentication);
+		Object principal = authentication.getPrincipal();
+		if (principal!=null) DEBUG_OUT.println("   principal: "+principal.getClass()+" -> "+principal);
+
+		if (principal instanceof OAuth2AuthenticatedPrincipal user) {
+			DEBUG_OUT.println("User Attributes:");
+			user.getAttributes().forEach((key, value) ->
+					DEBUG_OUT.println("   ["+key+"]: "+value+ (value==null ? "" : " { Class:"+value.getClass().getName()+" }"))
+			);
+			return new UserInfos(
+					true,
+					Objects.toString( user.getAttribute("id"), null ),
+					Objects.toString( user.getAttribute("login"), null ),
+					Objects.toString( user.getAttribute("name"), null ),
+					Objects.toString( user.getAttribute("location"), null ),
+					Objects.toString( user.getAttribute("url"), null ),
+					Objects.toString( user.getAttribute("avatar_url"), null )
+			);
+		}
+
+		return new UserInfos( false, authentication.getName(), null,null, null,null,null);
 	}
 }
---
+++ backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/security/UserInfos.java
@@ -0,0 +1,12 @@
+package net.schwarzbaer.spring.promptoptimizer.backend.security;
+
+public record UserInfos(
+		boolean isAuthenticated,
+		String id,
+		String login,
+		String name,
+		String location,
+		String url,
+		String avatar_url
+) {
+}
