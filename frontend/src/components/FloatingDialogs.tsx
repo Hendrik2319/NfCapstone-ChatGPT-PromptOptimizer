a18271ec774f863df0380d1a0d8a4286f494270b
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-26 12:35:49 +0200

    feat: [Frontend] added FloatingDialogs

---
+++ frontend/src/components/FloatingDialogs.tsx
@@ -0,0 +1,48 @@
+import './FloatingDialogs.css'
+import {ReactNode} from "react";
+import {DEBUG} from "../Types.tsx";
+
+export type DialogControl<DialogOptions> = {
+    closeDialog: ()=>void
+    setInitFunction: ( initFunction: (options:DialogOptions)=> void ) => void
+}
+
+export function createDialog<DialogOptions>( id:string, writeContent: ( dialogControl: DialogControl<DialogOptions> ) => ReactNode ) {
+
+    let initFunction: undefined | ((options:DialogOptions)=> void) = undefined
+
+    function showDialog( visible:boolean, options?:DialogOptions ) {
+        if (DEBUG) console.debug(`FloatingDialog[ ${id} ] -> showDialog( visible:${visible}, options:${options} )`)
+        const dialog = document.querySelector('#'+id)
+        if (dialog) {
+            if (visible) {
+                dialog.classList.add('visible')
+                if (initFunction && options)
+                    initFunction( options )
+            }
+            else
+                dialog.classList.remove('visible')
+        } else
+            console.error(`Can't find FloatingDialog[ ${id} ]`)
+    }
+
+    function closeDialog() {
+        showDialog(false)
+    }
+
+    function setInitFunction(initFunction_: (options:DialogOptions)=> void ) {
+        initFunction = initFunction_;
+    }
+
+    return {
+        showDialog : (options?:DialogOptions) => showDialog(true, options),
+        closeDialog,
+        writeHTML  : () => (
+            <div id={id} className="FloatingDialogBackground">
+                <div className="FloatingDialog">
+                    {writeContent({ closeDialog, setInitFunction })}
+                </div>
+            </div>
+        )
+    }
+}
