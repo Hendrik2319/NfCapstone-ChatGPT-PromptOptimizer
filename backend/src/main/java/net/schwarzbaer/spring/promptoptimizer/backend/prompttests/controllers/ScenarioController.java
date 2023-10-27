2975ee5d4dad6451bf55a263231061209280b04a
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-27 14:39:00 +0200

    feat: [Scenario3] added deleteScenario to ScenarioController

--- backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/prompttests/controllers/ScenarioController.java
+++ backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/prompttests/controllers/ScenarioController.java
@@ -22,48 +22,60 @@ public class ScenarioController {
 	private final ScenarioService scenarioService;
 
 	@GetMapping
-	public List<Scenario> getAllScenariosOfUser() {
+	public List<Scenario> getAllScenariosOfUser()
+	{
 		return scenarioService.getAllScenariosOfUser();
 	}
 
 	@GetMapping("all")
-	public List<Scenario> getAllScenarios() {
+	public List<Scenario> getAllScenarios()
+	{
 		return scenarioService.getAllScenarios();
 	}
 
 	@PostMapping
-	public ResponseEntity<Scenario> addScenarios(@RequestBody NewScenario newScenario) {
+	public ResponseEntity<Scenario> addScenarios(@RequestBody NewScenario newScenario)
+	{
 		return ResponseEntity.of(scenarioService.addScenarios(newScenario));
 	}
 
 	@PutMapping("{id}")
-	public ResponseEntity<Scenario> updateScenario(
-			@PathVariable String id,
-			@RequestBody Scenario scenario
-	) throws ScenarioService.UserIsNotAllowedException
+	public ResponseEntity<Scenario> updateScenario(@PathVariable String id,@RequestBody Scenario scenario)
+			throws ScenarioService.UserIsNotAllowedException
 	{
 		return ResponseEntity.of(scenarioService.updateScenario(id, scenario));
 	}
 
+	@DeleteMapping("{id}")
+	public void deleteScenario(@PathVariable String id)
+			throws ScenarioService.UserIsNotAllowedException
+	{
+		scenarioService.deleteScenario(id);
+	}
+
 	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorMessage handleException(IllegalArgumentException ex) {
+	public ErrorMessage handleException(IllegalArgumentException ex)
+	{
 		return getErrorMessageAndDoLog("IllegalArgumentException", ex);
 	}
 
 	@ExceptionHandler(ScenarioService.UserIsNotAllowedException.class)
 	@ResponseStatus(HttpStatus.FORBIDDEN)
-	public ErrorMessage handleException(ScenarioService.UserIsNotAllowedException ex) {
-		return getErrorMessageAndDoLog("IllegalArgumentException", ex);
+	public ErrorMessage handleException(ScenarioService.UserIsNotAllowedException ex)
+	{
+		return getErrorMessageAndDoLog("UserIsNotAllowedException", ex);
 	}
 
 	@ExceptionHandler(NoSuchElementException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ErrorMessage handleException(NoSuchElementException ex) {
+	public ErrorMessage handleException(NoSuchElementException ex)
+	{
 		return getErrorMessageAndDoLog("NoSuchElementException", ex);
 	}
 
-	private static ErrorMessage getErrorMessageAndDoLog(String exception, Exception ex) {
+	private static ErrorMessage getErrorMessageAndDoLog(String exception, Exception ex)
+	{
 		String message = "%s: %s".formatted(exception, ex.getMessage());
 		log.error(message);
 		return new ErrorMessage(message);
