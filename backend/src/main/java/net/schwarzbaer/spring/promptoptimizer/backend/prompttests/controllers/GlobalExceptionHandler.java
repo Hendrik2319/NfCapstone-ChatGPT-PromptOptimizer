13c9de8a8fcf968cd91baa3bf6de5c73c280cc5a
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-28 19:04:34 +0200

    feat: [TestRun] moved exception handlers into GlobalExceptionHandler

---
+++ backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/prompttests/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package net.schwarzbaer.spring.promptoptimizer.backend.prompttests.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import net.schwarzbaer.spring.promptoptimizer.backend.ErrorMessage;
+import net.schwarzbaer.spring.promptoptimizer.backend.prompttests.services.ScenarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorMessage handleException(IllegalArgumentException ex)
+	{
+		return getErrorMessageAndDoLog("IllegalArgumentException", ex);
+	}
+
+	@ExceptionHandler(ScenarioService.UserIsNotAllowedException.class)
+	@ResponseStatus(HttpStatus.FORBIDDEN)
+	public ErrorMessage handleException(ScenarioService.UserIsNotAllowedException ex)
+	{
+		return getErrorMessageAndDoLog("UserIsNotAllowedException", ex);
+	}
+
+	@ExceptionHandler(NoSuchElementException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ErrorMessage handleException(NoSuchElementException ex)
+	{
+		return getErrorMessageAndDoLog("NoSuchElementException", ex);
+	}
+
+	private static ErrorMessage getErrorMessageAndDoLog(String exception, Exception ex)
+	{
+		String message = "%s: %s".formatted(exception, ex.getMessage());
+		log.error(message);
+		return new ErrorMessage(message);
+	}
+}
--- backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/prompttests/controllers/ScenarioController.java
+++ backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/prompttests/controllers/ScenarioController.java
@@ -1,22 +1,17 @@
 package net.schwarzbaer.spring.promptoptimizer.backend.prompttests.controllers;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import net.schwarzbaer.spring.promptoptimizer.backend.ErrorMessage;
 import net.schwarzbaer.spring.promptoptimizer.backend.prompttests.models.NewScenario;
 import net.schwarzbaer.spring.promptoptimizer.backend.prompttests.models.Scenario;
 import net.schwarzbaer.spring.promptoptimizer.backend.prompttests.services.ScenarioService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/scenario")
 @RequiredArgsConstructor
-@Slf4j
 public class ScenarioController {
 
 	private final ScenarioService scenarioService;
@@ -51,34 +46,6 @@ public class ScenarioController {
 			throws ScenarioService.UserIsNotAllowedException
 	{
 		scenarioService.deleteScenario(id);
-	}
-
-	@ExceptionHandler(IllegalArgumentException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorMessage handleException(IllegalArgumentException ex)
-	{
-		return getErrorMessageAndDoLog("IllegalArgumentException", ex);
-	}
-
-	@ExceptionHandler(ScenarioService.UserIsNotAllowedException.class)
-	@ResponseStatus(HttpStatus.FORBIDDEN)
-	public ErrorMessage handleException(ScenarioService.UserIsNotAllowedException ex)
-	{
-		return getErrorMessageAndDoLog("UserIsNotAllowedException", ex);
-	}
-
-	@ExceptionHandler(NoSuchElementException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ErrorMessage handleException(NoSuchElementException ex)
-	{
-		return getErrorMessageAndDoLog("NoSuchElementException", ex);
-	}
-
-	private static ErrorMessage getErrorMessageAndDoLog(String exception, Exception ex)
-	{
-		String message = "%s: %s".formatted(exception, ex.getMessage());
-		log.error(message);
-		return new ErrorMessage(message);
 	}
 
 }
