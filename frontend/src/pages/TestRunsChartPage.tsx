67d9a130a0a525a04d55cb2c95c73bb944179eca
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-11-06 19:01:56 +0100

    feat: [OptimizationChart] extended chart

--- frontend/src/pages/TestRunsChartPage.tsx
+++ frontend/src/pages/TestRunsChartPage.tsx
@@ -1,29 +1,45 @@
 import BreadCrumbs from "../components/BreadCrumbs.tsx";
 import {useParams} from "react-router-dom";
-import {Line} from "react-chartjs-2";
+import {Chart} from "react-chartjs-2";
 import {
+    BarController,
+    BarElement,
     CategoryScale,
-    Chart as ChartJS,
+    Chart as ChartJS, ChartData,
     Legend,
     LinearScale,
+    LineController,
     LineElement,
     PointElement,
     Title,
     Tooltip
 } from "chart.js";
+import {SHOW_RENDERING_HINTS} from "../models/BaseTypes.tsx";
+import styled from "styled-components";
 
 ChartJS.register(
     CategoryScale,
     LinearScale,
+    BarElement,
     PointElement,
     LineElement,
+    LineController,
+    BarController,
     Title,
     Tooltip,
     Legend
 );
 
+const SimpleCard = styled.div`
+  border: 1px solid var(--border-color, #707070);
+  border-radius: 4px;
+  padding: 0.2em;
+  background: var(--background-color);
+`;
+
 export default function TestRunsChartPage() {
     const { id: scenarioId } = useParams();
+    if (SHOW_RENDERING_HINTS) console.debug("Rendering TestRunsChartPage", { scenarioId });
 
     if (!scenarioId)
         return <>No scenario choosen.</>
@@ -60,29 +76,37 @@ export default function TestRunsChartPage() {
 
     const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-    const data = {
-        labels,
+    const data: ChartData<"line" | "bar", number[], string> = {
+        labels: labels,
         datasets: [
             {
+                type: 'line' as const,
                 label: 'Dataset 1',
-                data: labels.map(() => Math.random()*2000 -1000),
+                data: labels.map(() => Math.random()*20 -10),
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderWidth: 2,
+                fill: false,
                 yAxisID: 'y',
             },
             {
+                type: 'bar' as const,
                 label: 'Dataset 2',
+                backgroundColor: 'rgb(75, 192, 192)',
                 data: labels.map(() => Math.random()*2000 -1000),
-                borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                borderColor: 'white',
+                borderWidth: 2,
                 yAxisID: 'y1',
             },
         ],
+        yLabels: [ 'A', 'B']
     };
     return (
         <>
             <BreadCrumbs scenarioId={scenarioId}/>
-            <Line options={options} data={data}/>
+            <SimpleCard>
+                <Chart type='bar' data={data} options={options}/>
+            </SimpleCard>
         </>
     )
 }
