84bc689563340039309de2f6348d11b50c4b80cf
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-11-06 18:16:51 +0100

    feat: [OptimizationChart] added example chart

--- frontend/src/pages/TestRunsChartPage.tsx
+++ frontend/src/pages/TestRunsChartPage.tsx
@@ -1,5 +1,6 @@
 import BreadCrumbs from "../components/BreadCrumbs.tsx";
 import {useParams} from "react-router-dom";
+import {Line} from "react-chartjs-2";
 
 export default function TestRunsChartPage() {
     const { id: scenarioId } = useParams();
@@ -7,10 +8,31 @@ export default function TestRunsChartPage() {
     if (!scenarioId)
         return <>No scenario choosen.</>
 
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: labels.map(() => Math.random()*2000 -1000),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                yAxisID: 'y',
+            },
+            {
+                label: 'Dataset 2',
+                data: labels.map(() => Math.random()*2000 -1000),
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                yAxisID: 'y1',
+            },
+        ],
+    };
     return (
         <>
             <BreadCrumbs scenarioId={scenarioId}/>
-            Content
+            <Line data={data}/>
         </>
     )
 }
