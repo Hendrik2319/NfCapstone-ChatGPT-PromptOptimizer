f32424dbd88e276572ae5bd3c89e552f4a1e1fd0
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-11-07 19:42:41 +0100

    feat: [UserManagement/Backend] added unit tests for new methods of UserService

--- backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/security/services/UserService.java
+++ backend/src/main/java/net/schwarzbaer/spring/promptoptimizer/backend/security/services/UserService.java
@@ -22,13 +22,14 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
 
-	private static final String ATTR_AVATAR_URL = "avatar_url";
-	private static final String ATTR_URL = "html_url";
-	private static final String ATTR_LOCATION = "location";
-	private static final String ATTR_NAME = "name";
-	private static final String ATTR_LOGIN = "login";
-	public static final String ATTR_USER_DB_ID = "UserDbId";
-	public static final String ATTR_ORIGINAL_ID = "id";
+	static final String ATTR_ORIGINAL_ID = "id";
+	static final String ATTR_USER_DB_ID  = "UserDbId";
+	static final String ATTR_LOGIN       = "login";
+	static final String ATTR_NAME        = "name";
+	static final String ATTR_LOCATION    = "location";
+	static final String ATTR_URL         = "html_url";
+	static final String ATTR_AVATAR_URL  = "avatar_url";
+
 	//	@SuppressWarnings("java:S106")
 //	private static final PrintStream DEBUG_OUT = System.out;
 
@@ -89,13 +90,7 @@ public class UserService {
 	}
 
 	public void updateUserIfNeeded(StoredUserInfo storedUserInfo, Map<String, Object> newAttributes) {
-		StoredUserInfo updatedUserInfo = updateUserObject(storedUserInfo, newAttributes);
-		if (updatedUserInfo.equals(storedUserInfo))
-			saveUser(updatedUserInfo);
-	}
-
-	private StoredUserInfo updateUserObject(StoredUserInfo storedUserInfo, Map<String, Object> newAttributes) {
-		return new StoredUserInfo(
+		StoredUserInfo updatedUserInfo = new StoredUserInfo(
 				storedUserInfo.id(),
 				storedUserInfo.role(),
 				storedUserInfo.registrationId(),
@@ -106,9 +101,7 @@ public class UserService {
 				Objects.toString(newAttributes.get(ATTR_URL        ), storedUserInfo.url       ()),
 				Objects.toString(newAttributes.get(ATTR_AVATAR_URL ), storedUserInfo.avatar_url())
 		);
-	}
-
-	private void saveUser(StoredUserInfo updatedUserInfo) {
-		userRepository.save(updatedUserInfo);
+		if (!updatedUserInfo.equals(storedUserInfo))
+			userRepository.save(updatedUserInfo);
 	}
 }
--- backend/src/test/java/net/schwarzbaer/spring/promptoptimizer/backend/security/services/UserServiceTest.java
+++ backend/src/test/java/net/schwarzbaer/spring/promptoptimizer/backend/security/services/UserServiceTest.java
@@ -1,7 +1,9 @@
 package net.schwarzbaer.spring.promptoptimizer.backend.security.services;
 
 import net.schwarzbaer.spring.promptoptimizer.backend.security.models.Role;
+import net.schwarzbaer.spring.promptoptimizer.backend.security.models.StoredUserInfo;
 import net.schwarzbaer.spring.promptoptimizer.backend.security.models.UserInfos;
+import net.schwarzbaer.spring.promptoptimizer.backend.security.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -17,14 +19,17 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
 	@Mock private SecurityContext securityContext;
 	@Mock private Authentication authentication;
+	@Mock private UserRepository userRepository;
 	@InjectMocks private UserService userService;
 
 	@BeforeEach
@@ -51,6 +56,10 @@ class UserServiceTest {
 		when(authentication.getName()).thenThrow(IllegalStateException.class);
 		SecurityContextHolder.setContext(securityContext);
 	}
+
+// ####################################################################################
+//               getCurrentUser
+// ####################################################################################
 
 	@Test
 	void whenGetCurrentUser_isCalledAndAuthenticationIsNull() {
@@ -88,6 +97,26 @@ class UserServiceTest {
 		assertEquals(expected, actual);
 	}
 
+	@Test void whenGetCurrentUser_isCalledByAuthenticatedUser() {
+		whenGetCurrentUser_isCalled(
+				null, "TestID2", "TestLogin",
+				false, false );
+	}
+	@Test void whenGetCurrentUser_isCalledByUnknownAccount() {
+		whenGetCurrentUser_isCalled(
+				Role.UNKNOWN_ACCOUNT, "TestID3", "TestUnknownAccount",
+				false, false );
+	}
+	@Test void whenGetCurrentUser_isCalledByUser() {
+		whenGetCurrentUser_isCalled(
+				Role.USER, "TestID4", "TestUser",
+				true, false );
+	}
+	@Test void whenGetCurrentUser_isCalledByAdmin() {
+		whenGetCurrentUser_isCalled(
+				Role.ADMIN, "TestID5", "TestAdmin",
+				false, true );
+	}
 	private void whenGetCurrentUser_isCalled(
 			Role loggedUserRole,
 			String loggedUserID,
@@ -108,35 +137,137 @@ class UserServiceTest {
 		assertEquals(expected, actual);
 	}
 
+// ####################################################################################
+//               getUserById
+// ####################################################################################
+
 	@Test
-	void whenGetCurrentUser_isCalledByAuthenticatedUser() {
-		whenGetCurrentUser_isCalled(
-				null, "TestID2", "TestLogin",
-				false, false
+	void whenGetUserById_getsUnknownId_returnsEmptyOptional() {
+		// Given
+		when(userRepository.findById(any())).thenReturn(
+				Optional.empty()
 		);
+
+		// When
+		Optional<StoredUserInfo> actual = userService.getUserById("userDbId");
+
+		// Then
+		verify(userRepository).findById("userDbId");
+		assertNotNull(actual);
+		assertTrue(actual.isEmpty());
 	}
 
 	@Test
-	void whenGetCurrentUser_isCalledByUnknownAccount() {
-		whenGetCurrentUser_isCalled(
-				Role.UNKNOWN_ACCOUNT, "TestID3", "TestUnknownAccount",
-				false, false
+	void whenGetUserById_getsKnownId_returnsOptionalWithData() {
+		// Given
+		when(userRepository.findById(any())).thenReturn(
+				Optional.of(new StoredUserInfo(
+						"RegistrationIduserID", Role.USER, "RegistrationId",
+						"userID", "login",
+						null, null, null, null
+				))
 		);
+
+		// When
+		Optional<StoredUserInfo> actual = userService.getUserById("RegistrationIduserID");
+
+		// Then
+		verify(userRepository).findById("RegistrationIduserID");
+		StoredUserInfo expected = new StoredUserInfo(
+				"RegistrationIduserID", Role.USER, "RegistrationId",
+				"userID", "login",
+				null, null, null, null
+		);
+		assertNotNull(actual);
+		assertTrue(actual.isPresent());
+		assertEquals(expected, actual.get());
 	}
 
-	@Test
-	void whenGetCurrentUser_isCalledByUser() {
-		whenGetCurrentUser_isCalled(
-				Role.USER, "TestID4", "TestUser",
-				true, false
-		);
-	}
+// ####################################################################################
+//               addUser
+// ####################################################################################
 
 	@Test
-	void whenGetCurrentUser_isCalledByAdmin() {
-		whenGetCurrentUser_isCalled(
-				Role.ADMIN, "TestID5", "TestAdmin",
-				false, true
+	void whenAddUser_isCalled() {
+		// Given
+		// When
+		Map<String, Object> attrs = Map.of(
+				"UserDbId"  , "RegistrationIDuserID1",
+				"id"        , "userID1"  ,
+				"login"     , "login1"   ,
+				"name"      , "name1"    ,
+				"location"  , "location1",
+				"html_url"  , "url1"     ,
+				"avatar_url", "avatarUrl1"
 		);
+		userService.addUser(Role.UNKNOWN_ACCOUNT, "RegistrationID", attrs);
+
+		// Then
+		verify(userRepository).save(new StoredUserInfo(
+				"RegistrationIDuserID1", Role.UNKNOWN_ACCOUNT, "RegistrationID",
+				"userID1", "login1",
+				"name1", "location1", "url1", "avatarUrl1"
+		));
+	}
+
+// ####################################################################################
+//               updateUserIfNeeded
+// ####################################################################################
+
+	@Test
+	void whenUpdateUserIfNeeded_isCalledWithNewData() {
+		// Given
+		// When
+		Map<String, Object> attrs = Map.of(
+				"UserDbId"  , "RegistrationIDuserID1",
+				"id"        , "userID1"  ,
+				"login"     , "login1"   ,
+				"name"      , "name1"    ,
+				"location"  , "location1",
+				"html_url"  , "url1"     ,
+				"avatar_url", "avatarUrl1"
+		);
+		StoredUserInfo storedUserInfo = new StoredUserInfo(
+				"RegistrationIDuserID1", Role.UNKNOWN_ACCOUNT, "RegistrationID",
+				"userID1", "login2",
+				"name2", "location2", "url2", "avatarUrl2"
+		);
+		userService.updateUserIfNeeded(storedUserInfo, attrs);
+
+		// Then
+		verify(userRepository).save(new StoredUserInfo(
+				"RegistrationIDuserID1", Role.UNKNOWN_ACCOUNT, "RegistrationID",
+				"userID1", "login1",
+				"name1", "location1", "url1", "avatarUrl1"
+		));
+	}
+
+	@Test void whenUpdateUserIfNeeded_isCalledWithNoData() {
+		whenUpdateUserIfNeeded_isCalled_andNothingIsWrittenToRepo(Map.of());
+	}
+	@Test void whenUpdateUserIfNeeded_isCalledWithSameData() {
+		whenUpdateUserIfNeeded_isCalled_andNothingIsWrittenToRepo(Map.of(
+				"UserDbId"  , "RegistrationIDuserID1",
+				"id"        , "userID1"  ,
+				"login"     , "login1"   ,
+				"name"      , "name1"    ,
+				"location"  , "location1",
+				"html_url"  , "url1"     ,
+				"avatar_url", "avatarUrl1"
+		));
+	}
+	private void whenUpdateUserIfNeeded_isCalled_andNothingIsWrittenToRepo(Map<String, Object> attrs) {
+		// Given
+
+		// When
+		StoredUserInfo storedUserInfo = new StoredUserInfo(
+				"RegistrationIDuserID1", Role.UNKNOWN_ACCOUNT, "RegistrationID",
+				"userID1", "login1",
+				"name1", "location1", "url1", "avatarUrl1"
+		);
+		userService.updateUserIfNeeded(storedUserInfo, attrs);
+
+		// Then
+		verify(userRepository, times(0)).save(any());
 	}
 }
